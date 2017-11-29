a93e543da3b7f452344fef4f43a935f51c024486
a_lebedev <a_lebedev@wargaming.net>
2017-11-29 15:17:07 +0300

    Full unit selection functionality

--- Assets/Sources/Systems/Piloting/SelectionSystem.cs
+++ Assets/Sources/Systems/Piloting/SelectionSystem.cs
@@ -1,49 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using Entitas;
 using Entitas.Unity;
 using System;
+using System.Linq;
 
 public class SelectionSystem : ReactiveSystem<InputEntity>
 {
-    IGroup<InputEntity> group;
+    IGroup<GameEntity> selectableGroup;
+    IGroup<InputEntity> pointerGroup;
     IGroup<GameEntity> selectedEntities;
-
     public SelectionSystem(Contexts contexts) : base(contexts.input)
     {
-        group = contexts.input.GetGroup(InputMatcher.InputPointerPosition);
+        selectableGroup = contexts.game.GetGroup(GameMatcher.AllOf(GameMatcher.Selectable, GameMatcher.View));
         selectedEntities = contexts.game.GetGroup(GameMatcher.Selected);
+        pointerGroup = contexts.input.GetGroup(InputMatcher.InputPointerPosition);
     }
 
     protected override void Execute(List<InputEntity> entities)
     {
-        InputEntity pointerPosition = group.Last();
-        foreach (var entity in entities)
+        foreach (var selectionEntity in entities)
         {
-            Ray mouseRay = Camera.main.ScreenPointToRay(pointerPosition.inputPointerPosition.position);
-            RaycastHit hit;
-            if (Physics.Raycast(mouseRay, out hit, float.PositiveInfinity, LayerMask.GetMask("Selectable")))
+            DeselectAll();
+            Rect selectionRect = selectionEntity.inputSelectionFrame.rect;
+            if (selectionRect.width < 2 || selectionRect.height < 2)
             {
-                GameEntity linkedEntity = (GameEntity)hit.collider.gameObject.GetEntityLink().entity;
-                if (linkedEntity != null)
-                {
-                    DeselectAll();
-                    linkedEntity.isSelected = true;
-                }
+                SelectByRay();
             }
+            else
+            {
+                SelectByFrame(selectionRect);
+            }
+
         }
     }
 
     protected override bool Filter(InputEntity entity)
     {
-        return entity.isInputActionEnded && entity.hasInputAction && entity.inputAction.action == InputAction.SELECT;
+        return entity.hasInputSelectionFrame;
     }
 
     protected override ICollector<InputEntity> GetTrigger(IContext<InputEntity> context)
     {
-        return context.CreateCollector(InputMatcher.InputActionEnded.Added());
+        return context.CreateCollector(InputMatcher.InputSelectionFrame.Added());
+    }
+
+    private void SelectByFrame(Rect selectionRect)
+    {
+        foreach (var selectable in selectableGroup)
+        {
+            Vector3 unitCenter = Camera.main.WorldToScreenPoint(selectable.view.gameObject.transform.position);
+            if (selectionRect.Contains(unitCenter))
+            {
+                selectable.isSelected = true;
+            }
+        }
+    }
+
+    private void SelectByRay()
+    {
+        InputEntity pointerPosition = pointerGroup.Last();
+        Ray mouseRay = Camera.main.ScreenPointToRay(pointerPosition.inputPointerPosition.position);
+        RaycastHit hit;
+        if (Physics.Raycast(mouseRay, out hit, float.PositiveInfinity, LayerMask.GetMask("Selectable")))
+        {
+            GameEntity linkedEntity = (GameEntity)hit.collider.gameObject.GetEntityLink().entity;
+            if (linkedEntity != null)
+            {                
+                linkedEntity.isSelected = true;
+            }
+        }
     }
 
     private void DeselectAll()
